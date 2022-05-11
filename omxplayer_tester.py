10fbf7a597b77cefc247152db41d25e58f2029f9
AurelienConil <conil.a@hotmail.fr>
2022-05-11 22:04:35 +0200

    omxplayer_tester

--- omxplayer_tester.py
+++ omxplayer_tester.py
@@ -4,19 +4,23 @@ from time import sleep
 from pathlib import Path
 import sys
 
+listOfArgs = ['--no-osd','--no-keys','-b','-o','hdmi'] # local mean audio local, can be replaced with hdmi
+  
 print("Starting /media/fat32/main.mp4")
 VIDEO_PATH = Path("/media/fat32/main.mp4")
 
-player = OMXPlayer(VIDEO_PATH)
+player = OMXPlayer(VIDEO_PATH,VIDEO_PATH,dbus_name='org.mpris.MediaPlayer2.omxplayer1',args=listOfArgs)
 
 sleep(5)
 print("Quit right now")
 player.quit()
 
 print("Starting /media/fat32/wait.mp4")
+         
+            
 VIDEO_PATH = Path("/media/fat32/wait.mp4")
 
-player = OMXPlayer(VIDEO_PATH)
+player = OMXPlayer(VIDEO_PATH,dbus_name='org.mpris.MediaPlayer2.omxplayer1',args=listOfArgs)
 
 sleep(5)
 print("Quit right now")
